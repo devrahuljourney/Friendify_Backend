948608407b997d72d92f504ab83e6ff9e2dff6c7
rahulkumarv269 <rahulkumarv269@gmail.com>
2024-06-05 17:11:45 +0530

    added socket io

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const profileRoutes = require('./route/Profile');
 const searchRoute = require('./route/Search');
 const cors = require("cors");
 
-
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const fileUpload = require("express-fileupload");
@@ -17,26 +16,31 @@ const fileUpload = require("express-fileupload");
 const PORT = process.env.PORT || 3000;
 const db = require("./config/database");
 const { cloudinaryConnect } = require("./config/cloudinary");
+const http = require("http");  
+const initializeSocketServer = require("./socket/socketIo");
+const server = http.createServer(app);  
+
 db.connect();
 
 app.use(express.json());
 app.use(cookieParser());
-//const originUrl = "http://localhost:3000"
-const originUrl = "https://friendify-alpha.vercel.app"
+const localhost = "http://localhost:3000";
+const originUrl = "https://friendify-alpha.vercel.app";
 
 app.use(
-	cors({
-		origin: originUrl,
-		credentials:true,
-	})
-)
+    cors({
+        origin: [localhost, originUrl],
+        credentials: true,
+    })
+);
 
 app.use(
-	fileUpload({
-		useTempFiles:true,
-		tempFileDir:"/tmp",
-	})
-)
+    fileUpload({
+        useTempFiles: true,
+        tempFileDir: "/tmp",
+    })
+);
+
 cloudinaryConnect();
 
 app.use("/api/v1/auth", userRoutes);
@@ -46,10 +50,66 @@ app.use("/api/v1/like", likeRoutes);
 app.use("/api/v1/profile", profileRoutes);
 app.use("/api/v1/search", searchRoute);
 
+initializeSocketServer(server);
 
-
-
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`App is running at ${PORT}`);
 });
+
+
+
+// const express = require("express");
+// const app = express();
+
+// const userRoutes = require("./route/User"); 
+// const postRoutes = require('./route/Post');
+// const commentRoutes = require('./route/Comment');
+// const likeRoutes = require('./route/Like');
+// const profileRoutes = require('./route/Profile');
+// const searchRoute = require('./route/Search');
+// const cors = require("cors");
+
+
+// const cookieParser = require("cookie-parser");
+// require("dotenv").config();
+// const fileUpload = require("express-fileupload");
+
+// const PORT = process.env.PORT || 3000;
+// const db = require("./config/database");
+// const { cloudinaryConnect } = require("./config/cloudinary");
+// db.connect();
+
+// app.use(express.json());
+// app.use(cookieParser());
+// //const originUrl = "http://localhost:3000"
+// const originUrl = "https://friendify-alpha.vercel.app"
+
+// app.use(
+// 	cors({
+// 		origin: originUrl,
+// 		credentials:true,
+// 	})
+// )
+
+// app.use(
+// 	fileUpload({
+// 		useTempFiles:true,
+// 		tempFileDir:"/tmp",
+// 	})
+// )
+// cloudinaryConnect();
+
+// app.use("/api/v1/auth", userRoutes);
+// app.use("/api/v1/post", postRoutes);
+// app.use("/api/v1/comment", commentRoutes);
+// app.use("/api/v1/like", likeRoutes);
+// app.use("/api/v1/profile", profileRoutes);
+// app.use("/api/v1/search", searchRoute);
+
+
+
+
+
+// app.listen(PORT, () => {
+//     console.log(`App is running at ${PORT}`);
+// });
---
+++ socket/socketIo.js
@@ -0,0 +1,34 @@
+const { Server } = require("socket.io");
+
+function initializeSocketServer(server) {
+    const localhost = "http://localhost:3000";
+    const originUrl = "https://friendify-alpha.vercel.app";
+
+    const io = new Server(server, {
+        cors: {
+            origin: [localhost, originUrl],
+            methods: ["GET", "POST"]  
+        }
+    });
+
+    io.on('connection', (socket) => {  
+        console.log("User is Connected");
+
+        socket.on('disconnect', () => {
+            console.log("user disconnected");
+        });
+
+        socket.on("likePost", (data) => {
+            const {userId , postId } = data
+            io.emit("updatePost", userId); 
+            console.log(data);
+        });
+
+        socket.on("commentPost", (data) => {
+            io.emit("updatePost", data); 
+            console.log(data);
+        });
+    });
+}
+
+module.exports = initializeSocketServer;
