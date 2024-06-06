7801bc6fb1c71f0d415efd0b15ea840734869945
rahulkumarv269 <rahulkumarv269@gmail.com>
2024-06-06 14:13:22 +0530

    remove access control from index js

--- index.js
+++ index.js
@@ -40,12 +40,7 @@ app.use(
         tempFileDir: "/tmp",
     })
 );
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-});
+
 
 cloudinaryConnect();
 
