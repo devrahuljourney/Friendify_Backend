81a67ad600f60af31e5b53fde7ac48f4cb34d035
rahulkumarv269 <rahulkumarv269@gmail.com>
2024-06-06 14:09:29 +0530

    added access control in index js

--- index.js
+++ index.js
@@ -40,6 +40,12 @@ app.use(
         tempFileDir: "/tmp",
     })
 );
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
 
 cloudinaryConnect();
 
